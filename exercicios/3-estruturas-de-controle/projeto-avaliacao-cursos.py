869cb30435694abfe1fef32e4b503cdf0a00b562
Kingx12x <cleberdaruadebaixo@gmail.com>
2025-08-07 19:24:41 +0000

    .

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -1,22 +1,30 @@
-# Nesse desafio você verificará dentro de uma lista se o item estar contido nela, caso verdadeiro deverá imprimir na tela essa informação, além disso deverá solicitar avaliação para o item e armazená-la em um dicionário.
-# 1. Crie uma lista com 5 diferentes cursos do LinkedIn Learning
-# 2. Crie 3 variáveis do tipo string e associe 1 curso a cada uma delas
-# 3. Crie um dicionário vazio para armazenar a nota do curso
-# 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
-# 5. Se o curso estiver na lista, solicite uma nota para avaliação
-# 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+competências = ['Python', 'PowerBI', 'SQL', 'R', 'Scrum' ]
 
-list = ['Python', 'PowerBI', 'SQL', 'R', 'Scrum' ]
-print(list)
 print('----------------------------------')
-linguagens_de_programacao = 'Python', 'R'
+linguagens_de_programacao = 'Python'
 data = 'SQL'
-tool = 'PowerBi', 'Scrum'
-print()
+tool = 'PowerBi'
 
-lista = { }
 
-if 'Python' está na (lista)
+Habilidades = {}
+
+if data in competências:
+  print(f"A competência {data}, está disponível. Avalie o curso")
+  Habilidades[data] = int(input(f'Qual seria a nota para esse curso?(0 a 5) '))
+
+if tool in competências:
+  print(f"A competência {tool}, está disponível. Avalie o curso")
+  Habilidades[tool] = int(input(f'Qual seria a nota para esse curso?(0 a 5) '))
+
+if linguagens_de_programacao in competências:
+  print(f"A competência {linguagens_de_programacao}, está disponível. Avalie o curso")
+  Habilidades[linguagens_de_programacao] = int(input(f'Qual seria a nota para esse curso?(0 a 5) '))
+else:
+  print("Você ainda não aprendeu essa habilidade")  
+
+print(Habilidades)
+
+
 
 
 
